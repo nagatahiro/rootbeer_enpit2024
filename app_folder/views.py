16d7f4798da42606597dee109dc8eca24e944590
nagatahira hiroyuki <e225740@ie.u-ryukyu.ac.jp>
2024-11-05 15:15:49 +0900

    add django

---
+++ app_folder/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+ 
+from django.views import View  
+  
+class SampleView(View):  
+	def get(self, request, *args, **kwargs):  
+		return render(request, 'app_folder/top_page.html')
+top_page = SampleView.as_view()
