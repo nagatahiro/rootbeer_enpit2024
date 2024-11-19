3e49cb688880c3fae5eca9d884e21bdb5b1726cb
nagatahira hiroyuki <e225740@ie.u-ryukyu.ac.jp>
2024-11-19 10:48:48 +0900

    update layout

--- app_folder/forms.py
+++ app_folder/forms.py
@@ -14,3 +14,7 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'    
