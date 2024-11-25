e85558d07a0ffa837a4496b8b49b7b685813883a
nagatahira hiroyuki <e225740@ie.u-ryukyu.ac.jp>
2024-11-25 12:52:33 +0900

    11/24

--- app_folder/forms.py
+++ app_folder/forms.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from django import forms
 
 class LoginForm(AuthenticationForm):
     """ログインフォーム"""
@@ -18,3 +19,9 @@ class SignUpForm(UserCreationForm):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'form-control'    
+
+
+class DutchTreatForm(forms.Form):
+    amount = forms.IntegerField(label="金額", min_value=0)
+    item_name = forms.CharField(label="商品名", max_length=100)
+    people = forms.IntegerField(label="人数", min_value=1)
