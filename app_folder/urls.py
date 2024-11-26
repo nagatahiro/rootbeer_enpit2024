dc9f7fd4d53507c09293ad5c912e19402ade708d
e225736 <mlikcoffee0730@gmail.com>
2024-11-26 14:55:02 +0900

    a

--- app_folder/urls.py
+++ app_folder/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
 from . import views
 
-app_name = 'app_folder'
+app_name = "app_folder"  # 名前空間を正しく設定
+
 urlpatterns = [
-    path('top_page/', views.top_page, name='top_page'),
-    path('top/', views.TopView.as_view(), name='top'),
-    path("home/", views.HomeView.as_view(), name="home"),
-    path("login/", views.LoginView.as_view(), name="login"),
-    path("logout/", views.LogoutView.as_view(), name="logout"),
+    path('', views.TopView.as_view(), name='top'),
+    path('home/', views.HomeView.as_view(), name='home'),
+    path('login/', views.LoginView.as_view(), name='login'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('signup/', views.SignUp.as_view(), name='signup'),
+    path('create-group/', views.CreateGroupView.as_view(), name='create_group'),
+    path('home/<int:group_id>/', views.GroupDetailView.as_view(), name='group_detail'),
+    path('add-friend/', views.AddFriendPageView.as_view(), name='add_friend'),  # フレンド追加用のURL
 ]
--- app_folder/views.py
+++ app_folder/views.py
@@ -1,28 +1,30 @@
-from django.shortcuts import render
-from django.views import View  
+from django.shortcuts import render, redirect
+from django.views import View
+from django.views.generic import TemplateView  
 from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
 from django.http import HttpResponseRedirect
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic.edit import CreateView
-from .forms import SignUpForm
 from django.contrib.auth import login
-from django.urls import reverse
+from .forms import SignUpForm
+from django.contrib import messages
+from django.contrib.auth.models import Group
 
 from . import forms
-  
-class SampleView(View):  
-	def get(self, request, *args, **kwargs):  
-		return render(request, 'app_folder/top_page.html')
-top_page = SampleView.as_view()
 
-
-class TopView(TemplateView):
+class TopView(TemplateView):  # TopView の定義
     template_name = "app_folder/top.html"
 
 class HomeView(LoginRequiredMixin, TemplateView):
-    template_name = "app_folder/home.html"
+    template_name = "home.html"
+
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        groups = self.request.user.rolegroup_set.all()
+        orgs = [group.org for group in groups]
+        context['orgs'] = orgs
+        return context
 
 class LoginView(LoginView):
     """ログインページ"""
@@ -35,14 +37,46 @@ class LogoutView(LoginRequiredMixin, LogoutView):
 
 class SignUp(CreateView):
     form_class = SignUpForm
-    template_name = "app_folder/signup.html" 
-    success_url = reverse_lazy('top')
+    template_name = "app_folder/signup.html"
+    success_url = reverse_lazy('app_folder:home')
 
     def form_valid(self, form):
-        user = form.save() # formの情報を保存
-        login(self.request, user) # 認証
-        self.object = user 
-        return HttpResponseRedirect(self.get_success_url()) # リダイレクト
-    def get_success_url(self):
-        # 成功後にリダイレクトする URL を指定
-        return reverse('app_folder:home')
+        user = form.save()
+        login(self.request, user)
+        self.object = user
+        return HttpResponseRedirect(self.get_success_url())
+
+class CreateGroupView(LoginRequiredMixin, TemplateView):
+    """グループ作成ページ"""
+    template_name = "app_folder/create_group.html"
+
+    def post(self, request, *args, **kwargs):
+        group_name = request.POST.get('group_name')
+        invited_friends = request.POST.getlist('friends')  # 選択されたフレンドのIDリスト
+        if group_name:
+            group, created = Group.objects.get_or_create(name=group_name)
+            # 仮のフレンド処理（選択されたフレンドIDを表示）
+            print(f"グループ '{group_name}' に招待されたフレンド: {invited_friends}")
+            messages.success(request, f"グループ '{group_name}' を作成しました。")
+        return HttpResponseRedirect(reverse('app_folder:home')) # ホーム画面にリダイレクト
+
+
+class AddFriendPageView(LoginRequiredMixin, TemplateView):
+    template_name = "app_folder/add_friend.html"
+
+    def post(self, request, *args, **kwargs):
+        # フレンド追加処理
+        friend_name = request.POST.get('friend_name')
+        print(f"フレンド '{friend_name}' を追加しました。")
+        return redirect('app_folder:home')
+    
+class GroupDetailView(LoginRequiredMixin, TemplateView):
+    """グループ詳細ページ"""
+    template_name = "app_folder/group_detail.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        group_id = self.kwargs.get('group_id')
+        group = Group.objects.get(id=group_id)
+        context['group'] = group
+        return context
