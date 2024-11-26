c27cbf1bde15ff855f36454f526b47bbe3f0a9ff
e225736 <mlikcoffee0730@gmail.com>
2024-11-26 16:18:07 +0900

    group

--- app_folder/views.py
+++ app_folder/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth import login
 from .forms import SignUpForm
 from django.contrib import messages
 from django.contrib.auth.models import Group
+from django.contrib.auth.models import User
 
 from . import forms
 
@@ -19,11 +20,9 @@ class TopView(TemplateView):  # TopView の定義
 class HomeView(LoginRequiredMixin, TemplateView):
     template_name = "app_folder/home.html"
 
-    def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        groups = self.request.user.groups.all()
-        orgs = [group.org for group in groups]
-        context['orgs'] = orgs
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['groups'] = Group.objects.all()  # すべてのグループを取得
         return context
 
 class LoginView(LoginView):
@@ -50,16 +49,41 @@ class CreateGroupView(LoginRequiredMixin, TemplateView):
     """グループ作成ページ"""
     template_name = "app_folder/create_group.html"
 
-    def post(self, request, *args, **kwargs):
-        group_name = request.POST.get('group_name')
-        invited_friends = request.POST.getlist('friends')  # 選択されたフレンドのIDリスト
-        if group_name:
-            group, created = Group.objects.get_or_create(name=group_name)
-            # 仮のフレンド処理（選択されたフレンドIDを表示）
-            print(f"グループ '{group_name}' に招待されたフレンド: {invited_friends}")
-            messages.success(request, f"グループ '{group_name}' を作成しました。")
-        return HttpResponseRedirect(reverse('app_folder:home')) # ホーム画面にリダイレクト
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # セッションから検索結果を取得してコンテキストに渡す
+        search_results = self.request.session.pop('search_results', [])
+        context['search_results'] = search_results
+        return context
 
+    def post(self, request, *args, **kwargs):
+        action = request.POST.get('action')
+
+        if action == "search":
+            # ユーザー検索処理
+            search_query = request.POST.get('search_user', '')
+            if search_query:
+                search_results = list(User.objects.filter(username__icontains=search_query).values('id', 'username'))
+                request.session['search_results'] = search_results
+            else:
+                request.session['search_results'] = []
+            return redirect('app_folder:create_group')
+
+        elif action == "create":
+            # グループ作成処理
+            group_name = request.POST.get('group_name')
+            invited_users_ids = request.POST.getlist('invited_users')  # 招待するユーザーIDリスト
+
+            if group_name:
+                group, created = Group.objects.get_or_create(name=group_name)
+                if invited_users_ids:
+                    invited_users = User.objects.filter(id__in=invited_users_ids)
+                    # ユーザーをグループに追加する処理
+                    print(f"招待されたユーザー: {[user.username for user in invited_users]}")
+                messages.success(request, f"グループ '{group_name}' を作成しました。")
+            return redirect('app_folder:home')
+        
+        
 
 class AddFriendPageView(LoginRequiredMixin, TemplateView):
     template_name = "app_folder/add_friend.html"
