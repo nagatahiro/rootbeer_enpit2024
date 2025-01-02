61b1be3715b082b9766b9f2cb1cc54663b841c95
nagatahiro <117128749+nagatahiro@users.noreply.github.com>
2025-01-02 12:20:43 +0900

    add host

--- app_config/settings.py
+++ app_config/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.environ["SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [ '127.0.0.1','waripoke.st.ie.u-ryukyu.ac.jp','https://waripoke.st.ie.u-ryukyu.ac.jp','http://waripoke.st.ie.u-ryukyu.ac.jp']
+ALLOWED_HOSTS = [ '127.0.0.1','waripoke.st.ie.u-ryukyu.ac.jp','https://waripoke.st.ie.u-ryukyu.ac.jp','http://waripoke.st.ie.u-ryukyu.ac.jp','http://waripoke.xyz','https://waripoke.xyz']
 
 
 # Application definition
