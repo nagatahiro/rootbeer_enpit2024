199fdbb8eb1db4bdb9b8e83b4916e744822db384
nagatahiro <117128749+nagatahiro@users.noreply.github.com>
2025-01-08 23:32:40 +0900

    Database creation completed

--- containers/django/entrypoint.sh
+++ containers/django/entrypoint.sh
@@ -1,5 +1,3 @@
-python manage.py migrate app_folder zero
-python manage.py migrate zero
 python manage.py makemigrations app_folder --no-input 
 python manage.py migrate --no-input
 python manage.py runserver 0.0.0.0:8000
